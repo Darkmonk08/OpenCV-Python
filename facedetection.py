5cf4c9fa611419c81293bfe8e91df52bc4d67703
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2026-03-01 16:27:39 +0530

    Add files via upload

---
+++ facedetection.py
@@ -0,0 +1,30 @@
+import cv2
+import os 
+
+folder="OpenCV-Python//faces"
+subfolder="raghav"
+
+path=os.path.join(folder,subfolder)
+if not os.path.isdir(path):
+    os.makedirs(path)
+    print("hello world")
+
+width=120
+height=140
+
+classifier=cv2.CascadeClassifier(r"C:\Users\ragha\OneDrive\Desktop\Jetlearn Python\OpenCv-Python\haarcascade_frontalface_default.xml")
+webcam=cv2.VideoCapture(0)
+picamnt=1
+while picamnt<=40:
+    print("hello")
+    status,frame=webcam.read()
+    if not status:
+        continue
+    gframe=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=classifier.detectMultiScale(gframe,1.3,4)
+    print(faces)
+    #picamnt=picamnt+1
+    cv2.imshow("feed",frame)
+    waitkey=cv2.waitKey(100)
+    if waitkey==27:
+        break

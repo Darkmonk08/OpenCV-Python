b51f48368b440bf72130828ee68a0b5e239d32fe
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2026-03-08 16:09:47 +0530

    facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -23,7 +23,13 @@ while picamnt<=40:
     gframe=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=classifier.detectMultiScale(gframe,1.3,4)
     print(faces)
-    #picamnt=picamnt+1
+    if len(faces)>0:
+        for x,y,w,h in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),4)
+            image=gframe[y:y+h,x:x+w]
+            resizedimage=cv2.resize(image,(width,height))
+            cv2.imwrite(os.path.join(path,f"{picamnt}.png"),resizedimage)
+        picamnt=picamnt+1
     cv2.imshow("feed",frame)
     waitkey=cv2.waitKey(100)
     if waitkey==27:
